5d271c0eb52bc745d75a23ec64c339177d900fc5
chanjman <mariozugaj@gmail.com>
2017-04-28 22:00:11 +0200

    Calculate username width and apply to dropdown

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -2,4 +2,8 @@ $(document).ready(function () {
   setTimeout(function () {
     $('#flash').remove();
   }, 10000);
+
+  var $usernameWidth = $('#dropbtn').css('width');
+  console.log($usernameWidth);
+  $('#dropdown-content').css('width', $usernameWidth);
 });
