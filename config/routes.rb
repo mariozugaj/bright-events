17da612434325d4283b9e64bb5c8dd5f7c48917e
chanjman <mariozugaj@gmail.com>
2017-04-29 10:07:28 +0200

    Change devise routes names

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   root 'static_pages#home'
-  devise_for :users
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login',
+                                                 sign_out: 'logout',
+                                                 password: 'secret',
+                                                 confirmation: 'verification',
+                                                 unlock: 'unblock',
+                                                 registration: 'register',
+                                                 sign_up: 'cmon_let_me_in' }
   get '/users/:id', to: 'users#show', as: 'user'
   resources :events
 end
