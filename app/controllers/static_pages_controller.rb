5ac8d7bcc50e068916dd069e6dce992b927109a1
chanjman <mariozugaj@gmail.com>
2017-05-06 12:57:16 +0200

    Add events near request.location to home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    @events = Event.upcoming(14)
+    @events = Event.upcoming.near(request.location)
   end
 end
