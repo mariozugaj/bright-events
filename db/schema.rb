ffea4995b87f1f1a10806c9fc72277d44a7c86e7
chanjman <mariozugaj@gmail.com>
2017-05-07 15:02:35 +0200

    New db schema for pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20170504140405) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "attendances", force: :cascade do |t|
     t.integer  "attended_event_id"
     t.integer  "attendee_id"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
-    t.index ["attended_event_id"], name: "index_attendances_on_attended_event_id"
-    t.index ["attendee_id"], name: "index_attendances_on_attendee_id"
+    t.index ["attended_event_id"], name: "index_attendances_on_attended_event_id", using: :btree
+    t.index ["attendee_id"], name: "index_attendances_on_attendee_id", using: :btree
   end
 
   create_table "categories", force: :cascade do |t|
@@ -41,15 +44,15 @@ ActiveRecord::Schema.define(version: 20170504140405) do
     t.string   "slug"
     t.string   "address"
     t.datetime "date_and_time"
-    t.index ["address"], name: "index_events_on_address"
-    t.index ["category_id"], name: "index_events_on_category_id"
-    t.index ["date_and_time"], name: "index_events_on_date_and_time"
-    t.index ["longitude", "latitude"], name: "index_events_on_longitude_and_latitude"
-    t.index ["picture"], name: "index_events_on_picture"
-    t.index ["slug"], name: "index_events_on_slug"
-    t.index ["title"], name: "index_events_on_title"
-    t.index ["user_id"], name: "index_events_on_user_id"
-    t.index ["venue"], name: "index_events_on_venue"
+    t.index ["address"], name: "index_events_on_address", using: :btree
+    t.index ["category_id"], name: "index_events_on_category_id", using: :btree
+    t.index ["date_and_time"], name: "index_events_on_date_and_time", using: :btree
+    t.index ["longitude", "latitude"], name: "index_events_on_longitude_and_latitude", using: :btree
+    t.index ["picture"], name: "index_events_on_picture", using: :btree
+    t.index ["slug"], name: "index_events_on_slug", using: :btree
+    t.index ["title"], name: "index_events_on_title", using: :btree
+    t.index ["user_id"], name: "index_events_on_user_id", using: :btree
+    t.index ["venue"], name: "index_events_on_venue", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -74,10 +77,12 @@ ActiveRecord::Schema.define(version: 20170504140405) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "slug"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
   end
 
+  add_foreign_key "events", "categories"
+  add_foreign_key "events", "users"
 end
