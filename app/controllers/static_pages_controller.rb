31b4ae87c612343e81b14b8e45b70e4e9b9f2fa9
chanjman <mariozugaj@gmail.com>
2017-05-07 20:55:02 +0200

    Add events near you to home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    @events = Event.upcoming
+    @events = Event.near(request.location).upcoming.limit(13)
   end
 end
