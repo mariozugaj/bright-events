c0f1822af53af64a556d70c5758137e363803a14
chanjman <mariozugaj@gmail.com>
2017-05-09 14:49:24 +0200

    Set ipapi.com as ip_lookup service

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,9 @@
   skip_before_action :authenticate_user!
 
   def home
-    request.env['REMOTE_ADDR'] = '31.147.121.212' if Rails.env.development?
+    request.env['REMOTE_ADDR'] = '185.137.18.54' if Rails.env.development?
     visitor_location = request.location
-    @events = Event.near(visitor_location.coordinates, 50).upcoming.limit(13)
+    @events = Event.near(visitor_location.coordinates, 50).upcoming.limit(15)
     @visitor_city = visitor_location.city
     @visitor_city_and_country = "#{@visitor_city}, #{visitor_location.country}"
     @categories = Category.pluck(:name, :id)
---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,8 @@
+Geocoder.configure(
+  units: :km,
+  google: {
+    use_https: true,
+    api_key: ENV['GMAPS_API_KEY']
+  },
+  ip_lookup: :ipapi_com
+)
