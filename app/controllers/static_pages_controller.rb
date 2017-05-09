e133c3a8d8c924c755470d43967350780a3cd716
chanjman <mariozugaj@gmail.com>
2017-05-09 11:21:53 +0200

    Add city name on home page to events near
    Add city name and link to query for given city

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,10 @@
-class StaticPagesController < ApplicationController
+  class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
     request.env['REMOTE_ADDR'] = '31.147.121.212' if Rails.env.development?
     visitor_location = request.safe_location
-    @events = Event.near(visitor_location.coordinates, 100).upcoming.limit(13)
+    @events = Event.near(visitor_location.coordinates, 50).upcoming.limit(13)
+    @visitor_city = visitor_location.city
   end
 end
