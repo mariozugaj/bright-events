c32f39a33b6275a54f7f6ad69f21a6e8c86a3204
chanjman <mariozugaj@gmail.com>
2017-08-28 15:14:02 +0200

    Events controller minor arrays cleanup

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
-  before_action :concatenate_date, only: [:create, :update]
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :concatenate_date, only: %i[create update]
+  before_action :correct_user, only: %i[edit update destroy]
 
   def index
     @events = search(params).paginate(page: params[:page])
